5bb00fca045deb18df9daf2088773aa5c5611f46
Leo Riesenbach <leoriesenbach@gmail.com>
2021-04-26 10:31:07 -0400

    switch to pros

--- src/main.cpp
+++ src/main.cpp
@@ -1,128 +1,92 @@
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// Controller1          controller                    
-// BackLeftDrive        motor         1               
-// BackRightDrive       motor         4               
-// FrontLeftDrive       motor         11              
-// FrontRightDrive      motor         20              
-// Shooter              motor         8               
-// Indexer              motor         15              
-// Inertial             inertial      2               
-// LeftIntake           motor         3               
-// RightIntake          motor         13              
-// LeftRotation         rotation      12              
-// RightRotation        rotation      10              
-// LineSensor           line          B               
-// Limit                limit         C               
-// ---- END VEXCODE CONFIGURED DEVICES ----
-/*----------------------------------------------------------------------------*/
-/*                                                                            */
-/*    Module:       main.cpp                                                  */
-/*    Author:       C:\Users\arybo                                            */
-/*    Created:      Fri Jul 17 2020                                           */
-/*    Description:  V5 project                                                */
-/*                                                                            */
-/*----------------------------------------------------------------------------*/
+#include "main.h"
 
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// Controller1          controller                    
-// BackLeftDrive        motor         1               
-// BackRightDrive       motor         4               
-// FrontLeftDrive       motor         11              
-// FrontRightDrive      motor         20              
-// Shooter               motor         8               
-// Indexer              motor         15              
-// Inertial             inertial      2               
-// LeftIntake           motor         3               
-// RightIntake          motor         13              
-// LeftRotation         rotation      12              
-// RightRotation        rotation      10              
-// LineSensor           line          B               
-// Limit                limit         C               
-// ---- END VEXCODE CONFIGURED DEVICES ----
-
-#include "vex.h"
-#include "opControl.h"
-#include "autonomous.h"
-#include "autonMethodLibrary.h"
-
-using namespace vex;
-
-// global instance of competition
-competition Competition;
-
-void pre_auton(void) {
-  // Initializing Robot Configuration. DO NOT REMOVE!
-  vexcodeInit();
-
-  Inertial.setHeading(270, degrees);
-  wait(2000, msec);
-
-  // All activities that occur before the competition starts
-  // Example: clearing encoders, setting servo positions, ...
+/**
+ * A callback function for LLEMU's center button.
+ *
+ * When this callback is fired, it will toggle line 2 of the LCD text between
+ * "I was pressed!" and nothing.
+ */
+void on_center_button() {
+	static bool pressed = false;
+	pressed = !pressed;
+	if (pressed) {
+		pros::lcd::set_text(2, "I was pressed!");
+	} else {
+		pros::lcd::clear_line(2);
+	}
 }
 
+/**
+ * Runs initialization code. This occurs as soon as the program is started.
+ *
+ * All other competition modes are blocked by initialize; it is recommended
+ * to keep execution time for this mode under a few seconds.
+ */
+void initialize() {
+	pros::lcd::initialize();
+	pros::lcd::set_text(1, "Hello PROS User!");
 
-void autonomous(void) {
-  // ..........................................................................
-  // Insert autonomous user code here.
-  thread t1(homeRow);
-  thread t2(positionTracking);
-
-  t1.join();
-  t2.join();
-  // ..........................................................................
+	pros::lcd::register_btn1_cb(on_center_button);
 }
 
+/**
+ * Runs while the robot is in the disabled state of Field Management System or
+ * the VEX Competition Switch, following either autonomous or opcontrol. When
+ * the robot is enabled, this task will exit.
+ */
+void disabled() {}
 
-void usercontrol(void) {
-  // User control code here, inside the loop
-  Controller1.ButtonR1.pressed(intakes::spinMaxIn);
-  Controller1.ButtonRight.pressed(intakes::spinMaxOut);
-  Controller1.ButtonR1.released(intakes::stopAll);
-  Controller1.ButtonRight.released(intakes::stopAll);
-  
-  while (1) {
-    joystickControl();
-    indexerControl();
-    shooterControl();
-    screenControl();
+/**
+ * Runs after initialize(), and before autonomous when connected to the Field
+ * Management System or the VEX Competition Switch. This is intended for
+ * competition-specific initialization routines, such as an autonomous selector
+ * on the LCD.
+ *
+ * This task will exit when the robot is enabled and autonomous or opcontrol
+ * starts.
+ */
+void competition_initialize() {}
 
-    // goalAlign();
-    wait(20, msec); // Sleep the task for a short amount of time to
-                     // prevent wasted resources.
-    if(Controller1.ButtonA.pressing()){
-      Inertial.setHeading(270, deg);
-      wait(2, sec);
-      thread t1(homeRow);
-      thread t2(positionTracking);
+/**
+ * Runs the user autonomous code. This function will be started in its own task
+ * with the default priority and stack size whenever the robot is enabled via
+ * the Field Management System or the VEX Competition Switch in the autonomous
+ * mode. Alternatively, this function may be called in initialize or opcontrol
+ * for non-competition testing purposes.
+ *
+ * If the robot is disabled or communications is lost, the autonomous task
+ * will be stopped. Re-enabling the robot will restart the task, not re-start it
+ * from where it left off.
+ */
+void autonomous() {}
 
-      t1.join();
-      t2.join();
-      }
-    // else if (Controller1.ButtonB.pressing()){
-    //   testQuadEquation(100, 80.00);
-    // }
-    wait(20, msec);
-  }
-}
+/**
+ * Runs the operator control code. This function will be started in its own task
+ * with the default priority and stack size whenever the robot is enabled via
+ * the Field Management System or the VEX Competition Switch in the operator
+ * control mode.
+ *
+ * If no competition control is connected, this function will run immediately
+ * following initialize().
+ *
+ * If the robot is disabled or communications is lost, the
+ * operator control task will be stopped. Re-enabling the robot will restart the
+ * task, not resume it from where it left off.
+ */
+void opcontrol() {
+	pros::Controller master(pros::E_CONTROLLER_MASTER);
+	pros::Motor left_mtr(1);
+	pros::Motor right_mtr(2);
 
-//
-// Main will set up the competition functions and callbacks.
-//
-int main() {
-  // Set up callbacks for autonomous and driver control periods.
-  Competition.autonomous(autonomous);
-  Competition.drivercontrol(usercontrol);
+	while (true) {
+		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
+		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
+		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
+		int left = master.get_analog(ANALOG_LEFT_Y);
+		int right = master.get_analog(ANALOG_RIGHT_Y);
 
-  // Run the pre-autonomous function.
-  pre_auton();
-
-  // Prevent main from exiting with an infinite loop.
-  while (true) {
-    wait(100, msec);
-  }
+		left_mtr = left;
+		right_mtr = right;
+		pros::delay(20);
+	}
 }
