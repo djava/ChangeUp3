e85a8c2dedb51caa3e39502c246fba640b8047cd
Leo Riesenbach <leoriesenbach@gmail.com>
2021-04-28 10:12:16 -0400

    falling and rising edge triggerfactory filters

--- include/lib/event.h
+++ include/lib/event.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <algorithm>
 #include <type_traits>
+#include <utility>
 #include <vector>
 #include <functional>
 
@@ -30,7 +31,7 @@ namespace lib {
         static void taskTriggerFunction();
     };
 
-    namespace triggerFactory {
+    namespace triggers {
         using less = std::less<>;
         using greater = std::greater<>;
         using less_equal = std::less_equal<>;
@@ -64,5 +65,50 @@ namespace lib {
             return [a = std::forward<A>(a), b = std::forward<B>(b), cmp{C {}}] { return cmp(a, b); };
         }
 
+        template <typename A, typename B,
+                  typename std::enable_if_t<std::is_invocable_v<A>>* = nullptr>
+        auto rangeTrigger(A&& a, B&& lower, B&& upper) {
+            return [a = std::forward<A>(a), lower = std::forward<B>(lower), upper = std::forward<B>(upper)]
+                    { return a() <= upper && a() >= lower; };
+        }
+
+        template <typename A, typename B,
+                  typename std::enable_if_t<!std::is_invocable_v<A>>* = nullptr>
+        auto rangeTrigger(A&& a, B&& lower, B&& upper) {
+            return [a = std::forward<A>(a), lower = std::forward<B>(lower), upper = std::forward<B>(upper)]
+                    { return a <= upper && a >= lower; };
+        }
+        
+        template <bool B, typename A,
+                  typename std::enable_if_t<std::is_invocable_v<A>>* = nullptr>
+        auto boolTrigger(A&& a) {
+            return [a = std::forward<A>(a), b = std::forward<bool>(B)] { return a() == b; };
+        }
+
+        template <bool B, typename A,
+                  typename std::enable_if_t<!std::is_invocable_v<A>>* = nullptr>
+        auto boolTrigger(A&& a) {
+            return [a = std::forward<A>(a), b = std::forward<bool>(B)] { return a == b; };
+        }
+
+        inline namespace filters {
+            template <typename A, typename std::enable_if_t<std::is_invocable_v<A>>* = nullptr>
+            auto risingEdgeFilter(A&& a) {
+                return [a = std::forward<A>(a)]{ 
+                    static bool on = false;
+                    bool oldOn = on; on = a();
+                    return on && !oldOn;
+                };
+            }
+            
+            template <typename A, typename std::enable_if_t<std::is_invocable_v<A>>* = nullptr>
+            auto fallingEdgeFilter(A&& a) {
+                return [a = std::forward<A>(a)]{ 
+                    static bool on = false;
+                    bool oldOn = on; on = a();
+                    return !on && oldOn;
+                };
+            }
+        }
     } // namespace triggerFactory
 } // namespace lib
